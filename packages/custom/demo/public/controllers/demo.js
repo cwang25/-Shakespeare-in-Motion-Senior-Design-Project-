2ed14040be55ee6a35f69584850fbcd74898999a
Hans <cwang25@ncsu.edu>
2015-09-15 13:24:26 -0400

    Got the REST API calls working to get mock data from the database to the front-end.

--- packages/custom/demo/public/controllers/demo.js
+++ packages/custom/demo/public/controllers/demo.js
@@ -1,11 +1,34 @@
 'use strict';
 
 /* jshint -W098 */
-angular.module('mean.demo').controller('DemoController', ['$scope', 'Global', 'Demo',
-  function($scope, Global, Demo) {
+angular.module('mean.demo').controller('DemoController', ['$scope', 'Global', 'Demo', '$http',
+  function($scope, Global, Demo, $http) {
     $scope.global = Global;
     $scope.package = {
       name: 'demo'
     };
+    var refresh = function() {
+      $http.get('/api/demo/newsarticles').success(function (response) {
+        console.log("I got the data I requested");
+        $scope.newsarticles = response;
+        $scope.news = "";
+
+      });
+
+    };
+    refresh();
+
+    $scope.addNews = function() {
+      console.log($scope.news);
+      $http.post('/api/demo/newsarticles', $scope.news).success(function(response) {
+        console.log(response);
+        refresh();
+      });
+    };
+
   }
 ]);
+
+
+
+
