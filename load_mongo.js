d6c7ffc7174af28696b5ce7e6942a0d0cf4d96f9
Samuel Chetty <sjchetty@ncsu.edu>
2015-10-05 00:18:21 -0400

    Added a file called load_mongo.js to the project directory. This file loads the mean-test database with dummy quotes used for testing. I also modified the test.js files in the config/env and config/gulp directories so that, if you run gulp in test mode, it basically runs the same as it would in development mode, but it connects to the mean-test database instead of the mean-dev database. To run the server in the testing environment, type gulp test. To load the testing database, launch mongo on the command-line by typing mongo localhost/mean-test load-mongo.js

---
+++ load_mongo.js
@@ -0,0 +1,89 @@
+db.quotes.insert({
+    high : 87.00,
+    volume : 0,
+    low : 85.00,
+    qdate : ISODate('2015-10-05T00:00:00Z'),
+    close : 86.00,
+    symbol : 'DJC',
+    open : 86.00
+
+});
+
+db.quotes.insert({
+    high : 87.00,
+    volume : 0,
+    low : 85.00,
+    qdate : ISODate('2015-10-10T00:00:00Z'),
+    close : 86.00,
+    symbol : 'BCOM',
+    open : 86.00
+
+});
+
+
+db.quotes.insert({
+    high : 87.00,
+    volume : 0,
+    low : 85.00,
+    qdate : ISODate('2015-10-04T00:00:00Z'),
+    close : 86.00,
+    symbol : 'BCOM',
+    open : 86.00
+
+});
+
+
+db.quotes.insert({
+    high : 87.00,
+    volume : 0,
+    low : 85.00,
+    qdate : ISODate('2015-10-05T00:00:00Z'),
+    close : 86.00,
+    symbol : 'BCOM',
+    open : 86.00
+
+});
+
+db.quotes.insert({
+    high : 87.00,
+    volume : 0,
+    low : 85.00,
+    qdate : ISODate('2015-10-06T00:00:00Z'),
+    close : 84.00,
+    symbol : 'BCOM',
+    open : 86.00
+
+});
+
+db.quotes.insert({
+    high : 87.00,
+    volume : 0,
+    low : 85.00,
+    qdate : ISODate('2015-10-07T00:00:00Z'),
+    close : 88.00,
+    symbol : 'BCOM',
+    open : 86.00
+
+});
+
+db.quotes.insert({
+    high : 87.00,
+    volume : 0,
+    low : 85.00,
+    qdate : ISODate('2015-10-08T00:00:00Z'),
+    close : 87.00,
+    symbol : 'BCOM',
+    open : 86.00
+
+});
+
+db.quotes.insert({
+    high : 87.00,
+    volume : 0,
+    low : 85.00,
+    qdate : ISODate('2015-10-09T00:00:00Z'),
+    close : 85.00,
+    symbol : 'BCOM',
+    open : 86.00
+
+});
