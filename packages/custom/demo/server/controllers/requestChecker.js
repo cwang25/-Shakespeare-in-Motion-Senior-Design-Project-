b55bf2f9b3f6cf2445d3930218943894a407f491
Chi-Han Wang <cwang25@ncsu.edu>
2015-10-13 14:21:10 -0400

    Fixed Request Checker - Chi-Han Wang

--- packages/custom/demo/server/controllers/requestChecker.js
+++ packages/custom/demo/server/controllers/requestChecker.js
@@ -6,12 +6,13 @@ var _ = require('lodash');
 module.exports = {
   check_local: function (req, res, next) {
     var is_local = _.isEqual(ipaddr.process(req.ip), ipaddr.process('127.0.0.1'));
-    if (is_local) {
+    var is_local_v6 = _.isEqual(ipaddr.process(req.ip), ipaddr.process('::1'));
+    if (is_local || is_local_v6) {
       //console.log("true");
       return true;
     } else {
       //console.log("false");
-      res.send("Error! You are trying to access the private api but you are not authenticated client.");
+      res.send("Error! You are trying to access the private api but you are not authenticated client. Request from: "+ipaddr.process(req.ip));
       return false;
     }
   }
