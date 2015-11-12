043319c6177c00639a880a4ed1b2f0c280a2d63e
sjchetty <sjchetty@ncsu.edu>
2015-11-11 21:37:38 -0500

    Updated quotes in test data

--- load_mongo.js
+++ load_mongo.js
@@ -1,128 +1,281 @@
 db.quotes.remove({});
 
 db.quotes.insert({
-    high : 87.00,
+    high : 89.019997,
     volume : 0,
-    low : 85.00,
-    qdate : ISODate('2015-10-02T00:00:00Z'),
-    close : 84.9,
-    qsymbol : 'BCOM',
-    open : 86.00
-
-});
-
-
-
-db.quotes.insert({
-    high : 87.00,
-    volume : 0,
-    low : 85.00,
+    low : 88.139999,
     qdate : ISODate('2015-10-05T00:00:00Z'),
-    close : 86.00,
-    qsymbol : 'BCOM',
-    open : 86.00
+    close : 88.800003,
+    qsymbol : '^DJC',
+    open : 88.160004
 
 });
 
 db.quotes.insert({
-    high : 87.00,
+    high : 90.389999,
     volume : 0,
-    low : 85.00,
+    low : 88.43,
     qdate : ISODate('2015-10-06T00:00:00Z'),
-    close : 84.00,
-    qsymbol : 'BCOM',
-    open : 86.00
+    close : 90.269997,
+    qsymbol : '^DJC',
+    open : 88.75
 
 });
 
 db.quotes.insert({
-    high : 87.00,
+    high : 91.260002,
     volume : 0,
-    low : 85.00,
+    low : 90,
     qdate : ISODate('2015-10-07T00:00:00Z'),
-    close : 88.00,
-    qsymbol : 'BCOM',
-    open : 86.00
+    close : 90.110001 ,
+    qsymbol : '^DJC',
+    open : 90.410004
 
 });
 
 db.quotes.insert({
-    high : 87.00,
+    high : 90.550003,
     volume : 0,
-    low : 85.00,
+    low : 89.599998,
     qdate : ISODate('2015-10-08T00:00:00Z'),
-    close : 87.00,
-    qsymbol : 'BCOM',
-    open : 86.00
+    close : 90.349998,
+    qsymbol : '^DJC',
+    open : 89.910004
 
 });
 
 db.quotes.insert({
-    high : 87.00,
+    high : 91.940002,
     volume : 0,
-    low : 85.00,
+    low : 90.449997,
     qdate : ISODate('2015-10-09T00:00:00Z'),
-    close : 85.00,
-    qsymbol : 'BCOM',
-    open : 86.00
+    close : 91.010002,
+    qsymbol : '^DJC',
+    open : 90.459999
+
+});
+
+db.quotes.insert({
+    high : 91.68,
+    volume : 0,
+    low : 90.040001,
+    qdate : ISODate('2015-10-12T00:00:00Z'),
+    close : 90.050003,
+    qsymbol : '^DJC',
+    open : 91.230003
+
+});
+
+db.quotes.insert({
+    high : 90.580002,
+    volume : 0,
+    low : 89.599998,
+    qdate : ISODate('2015-10-13T00:00:00Z'),
+    close : 89.980003,
+    qsymbol : '^DJC',
+    open : 90.110001
+
+});
+
+db.quotes.insert({
+    high : 90.370003,
+    volume : 0,
+    low : 89.650002,
+    qdate : ISODate('2015-10-14T00:00:00Z'),
+    close : 90.139999 ,
+    qsymbol : '^DJC',
+    open : 89.959999
+
+});
+
+db.quotes.insert({
+    high : 90.339996,
+    volume : 0,
+    low : 89.389999,
+    qdate : ISODate('2015-10-15T00:00:00Z'),
+    close : 89.839996,
+    qsymbol : '^DJC',
+    open : 90.18
+
+});
+
+db.quotes.insert({
+    high : 89.970001,
+    volume : 0,
+    low : 89.110001,
+    qdate : ISODate('2015-10-16T00:00:00Z'),
+    close : 89.779999,
+    qsymbol : '^DJC',
+    open : 89.949997
+
+});
+
+db.quotes.insert({
+    high : 89.629997,
+    volume : 0,
+    low : 88.510002,
+    qdate : ISODate('2015-10-19T00:00:00Z'),
+    close : 88.519997,
+    qsymbol : '^DJC',
+    open : 89.510002
+
+});
+
+db.quotes.insert({
+    high : 89.099998,
+    volume : 0,
+    low : 88.379997,
+    qdate : ISODate('2015-10-20T00:00:00Z'),
+    close : 88.830002,
+    qsymbol : '^DJC',
+    open : 88.550003
+
+});
+
+db.quotes.insert({
+    high : 88.779999,
+    volume : 0,
+    low : 87.809998,
+    qdate : ISODate('2015-10-21T00:00:00Z'),
+    close : 88.18 ,
+    qsymbol : '^DJC',
+    open : 88.699997
+
+});
+
+db.quotes.insert({
+    high : 88.989998,
+    volume : 0,
+    low : 88.029999,
+    qdate : ISODate('2015-10-22T00:00:00Z'),
+    close : 88.169998,
+    qsymbol : '^DJC',
+    open : 88.279999
+
+});
+
+db.quotes.insert({
+    high : 88.739998,
+    volume : 0,
+    low : 87.25,
+    qdate : ISODate('2015-10-23T00:00:00Z'),
+    close : 87.449997,
+    qsymbol : '^DJC',
+    open : 88.239998
 
 });
 
 
 db.quotes.insert({
-    high : 87.00,
+    high : 87.599998,
     volume : 0,
-    low : 85.00,
-    qdate : ISODate('2015-10-05T00:00:00Z'),
-    close : 86.00,
+    low : 86.940002,
+    qdate : ISODate('2015-10-26T00:00:00Z'),
+    close : 86.959999,
     qsymbol : '^DJC',
-    open : 86.00
+    open : 87.43
 
 });
 
 db.quotes.insert({
-    high : 87.00,
+    high : 87.089996,
     volume : 0,
-    low : 85.00,
-    qdate : ISODate('2015-10-06T00:00:00Z'),
-    close : 87.00,
+    low : 86.489998,
+    qdate : ISODate('2015-10-27T00:00:00Z'),
+    close : 86.620003,
     qsymbol : '^DJC',
-    open : 86.00
+    open : 86.93
 
 });
 
 db.quotes.insert({
-    high : 87.00,
+    high : 87.760002,
     volume : 0,
-    low : 85.00,
-    qdate : ISODate('2015-10-07T00:00:00Z'),
-    close : 86.5,
+    low : 86.470001,
+    qdate : ISODate('2015-10-28T00:00:00Z'),
+    close : 87.639999 ,
     qsymbol : '^DJC',
-    open : 86.00
+    open : 86.629997
 
 });
 
 db.quotes.insert({
-    high : 87.00,
+    high : 87.589996,
     volume : 0,
-    low : 85.00,
-    qdate : ISODate('2015-10-08T00:00:00Z'),
-    close : 86.2,
+    low : 86.730003,
+    qdate : ISODate('2015-10-29T00:00:00Z'),
+    close : 86.870003,
     qsymbol : '^DJC',
-    open : 86.00
+    open : 87.309998
 
 });
 
 db.quotes.insert({
-    high : 87.00,
+    high : 87.629997,
     volume : 0,
-    low : 85.00,
-    qdate : ISODate('2015-10-09T00:00:00Z'),
-    close : 86.1,
+    low : 86.68,
+    qdate : ISODate('2015-10-30T00:00:00Z'),
+    close : 87.43,
     qsymbol : '^DJC',
-    open : 86.00
+    open : 86.839996
 
 });
+
+db.quotes.insert({
+    high : 104.769997,
+    volume : 0,
+    low : 86.620003,
+    qdate : ISODate('2015-11-02T00:00:00Z'),
+    close : 86.870003,
+    qsymbol : '^DJC',
+    open : 87.230003
+
+});
+
+db.quotes.insert({
+    high : 87.900002,
+    volume : 0,
+    low : 86.809998,
+    qdate : ISODate('2015-11-03T00:00:00Z'),
+    close : 87.690002,
+    qsymbol : '^DJC',
+    open : 87.010002
+
+});
+
+db.quotes.insert({
+    high : 88.129997,
+    volume : 0,
+    low : 86.5,
+    qdate : ISODate('2015-11-04T00:00:00Z'),
+    close : 86.510002 ,
+    qsymbol : '^DJC',
+    open : 87.839996
+
+});
+
+db.quotes.insert({
+    high : 86.660004,
+    volume : 0,
+    low : 85.800003,
+    qdate : ISODate('2015-11-05T00:00:00Z'),
+    close : 85.830002,
+    qsymbol : '^DJC',
+    open : 86.660004
+
+});
+
+db.quotes.insert({
+    high : 86.18,
+    volume : 0,
+    low : 84.910004,
+    qdate : ISODate('2015-11-06T00:00:00Z'),
+    close : 85.230003,
+    qsymbol : '^DJC',
+    open : 85.989998
+});
+
+
 
 db.newsarticles.remove({});
 
